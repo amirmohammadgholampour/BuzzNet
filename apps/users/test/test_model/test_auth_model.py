e17f2bc57af8965a60f6250dba27ef15b35c82b9
amirmohammadgholampour <amirmohammadghlmpr@gmail.com>
2025-09-04 18:32:21 +0330

    Create test with pytest for auth_model

---
+++ apps/users/test/test_model/test_auth_model.py
@@ -0,0 +1,58 @@
+import pytest 
+from apps.users.models import User, UserManager
+
+@pytest.fixture
+def user_a(db):
+    return User.objects.create_user(
+        email="user_a@test.com",
+        password="password123",
+        username="user_a",
+        full_name="User A"
+    )
+
+@pytest.fixture
+def user_b(db):
+    return User.objects.create_user(
+        email="user_b@test.com",
+        password="password456",
+        username="user_b",
+        full_name="User B"
+    )
+    
+@pytest.mark.django_db
+def test_user_creation(user_a):
+    """
+    =============
+    User A is created;
+    =============
+    """
+    assert user_a.username == "user_a"
+    assert user_a.email == "user_a@test.com"
+    assert user_a.check_password("password123")
+
+
+@pytest.mark.django_db
+def test_user_following(user_a, user_b):
+    """
+    =============
+    User A is not following to User B;
+    =============
+    """
+    assert not user_a.is_following(user_b)
+
+    """
+    =============
+    User A is started following to User B;
+    =============
+    """
+    user_a.follow(user_b)
+    assert user_a.is_following(user_b)
+    assert user_b.following.filter(id=user_a.id).exists() == False
+
+    """
+    =============
+    User A is stop following User B;
+    =============
+    """
+    user_a.unfollow(user_b)
+    assert not user_a.is_following(user_b)
